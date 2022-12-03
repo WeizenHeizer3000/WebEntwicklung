d72ea4c60d6024ac339ab63a3630deadc207ad42
WeizenHeizer3000 <s.ohs1@gmx.de>
2022-12-03 09:53:01 +0100

    Auslagerung Reiter

---
+++ menu.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://unpkg.com/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
+</head>
+<body>
+<ul class="list-group">
+    <li class="list-group-item">
+        <a class="text-decoration-none" href="#">Login</a>
+    </li>
+    <li class="list-group-item">
+        <a class="text-decoration-none" href="#">Projekte</a>
+    </li>
+    <li class="list-group-item">
+        <a class="text-decoration-none" href="#">Aktuelles Projekt</a>
+    </li>
+    <li class="list-group-item ms-4">
+        <a class="text-decoration-none" href="#">Reiter</a>
+    </li>
+    <li class="list-group-item ms-4">
+        <a class="text-decoration-none" href="#">Aufgaben</a>
+    </li>
+    <li class="list-group-item ms-4">
+        <a class="text-decoration-none" href="#">Mitglieder</a>
+    </li>
+</ul>
+</body>
