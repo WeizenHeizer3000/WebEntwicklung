ee673bb37afc40b4a1f99f0ea80d80790fe50d14
WeizenHeizer3000 <s.ohs1@gmx.de>
2022-12-03 11:53:37 +0100

    Erstellen der Tabelle bei Personen

--- mitglieder.php
+++ mitglieder.php
@@ -1,27 +1,62 @@
+<?php
+
+$mitglieder = array(
+    array(
+        'Name' => 'Max Mustermann',
+        'E-Mail' => 'mustermann@muster.de',
+        'In Projekt' => True,
+    ),
+    array(
+        'Name' => 'Petra Müller',
+        'E-Mail' => 'petra@mueller.de',
+        'In Projekt' => True,
+    )
+);
+
+function make_tbl($mitglieder){
+
+    $tbl_array = [];
+    $tbl_array[] = "<table>";
+    foreach( $mitglieder as $row){
+        $tbl_array[] = "<tr>";
+        foreach ( $row as $cell ) {
+            $tbl_array[] = "<td>$cell</td>";
+        }
+        $tbl_array[] = "</tr>";
+    }
+    $tbl_array[] = "</table>";
+
+    #return table as a string
+    return implode('', $tbl_array);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>ToDo Liste</title>
     <link href="https://unpkg.com/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="https://unpkg.com/bootstrap-table@1.20.1/dist/bootstrap-table.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="container-fluid">
     <header class="bg-light mb-3 mt-4 p-5">
-        <h1 class="display-5 text-center">Aufgabenplaner: Todos (Aktuelles Projekt)</h1>
+        <h1 class="display-5 text-center">Aufgabenplaner: Personen</h1>
     </header>
     <div class="row mt-4">
         <div class="col-2">
             <?php include("menu.php");?>
         </div>
-        <div><div class="col-8">
+        <div class="col-8">
                 <?php
-                    include 'mitglieder_array';
-                    foreach ($mitglieder as $mitglied) {
-                        echo ($mitglied['Name'].'<br>');
-                        echo ($mitglied['E-Mail'].'<br>');
-                        echo ($['In Projekt'].<br>');
-                    echo '<br/>';
+                     #               foreach ($mitglieder as $mitglieder) {
+                     #                   echo($mitglieder['Name'] . '<br>');
+                     #                   echo($mitglieder['E-Mail'] . '<br>');
+                     #                   echo($mitglieder['In Projekt'] . '<br>');
+                     #                   echo '<br/>';
+                     #               }
+                    make_tbl($mitglieder);
                 ?>
         </div>
             <div class="col-2">
